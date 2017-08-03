fbe3f01e94c0112fe1facda56f867b4d8b283fa7
ajamoranto <30665360+ajamoranto@users.noreply.github.com>
2017-08-03 07:08:35 -0700

    Testing

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,26 @@
 $(function() {
+  $.ajax({
+    url:https://www.codeschool.com/users/2653763.json,
+    dataType: 'jsonp',
+    success: function(response{
+      console.log('response', response.courses.completed)
+      addCourses(response.courses.completed);
+    })
+  });
+function addCourses(courses)  {
 
-  // your code will go here
+  var $badges = $('badges');
+
+  courses.forEach(function(course){
+    var $course = $('<div />', {
+      'class': 'course'
+    }).appendTo($badges);
+
+    $('<h3 />', ) {
+      text: course.title
+    }).appendTo($course);
+
+  })
+}
 
 });
